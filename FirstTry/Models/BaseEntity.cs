e24e9e5d18dd09728504bafa4ae591f04f109221
pedromaximiliano <pedro.maximiliaan@gmail.com>
2021-08-05 01:39:39 -0300

    Initial configuration

---
+++ FirstTry/Models/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstTry.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ FirstTry/Models/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstTry.Models
+{
+    public class ToDo : BaseEntity
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        [MaxLength(100)]
+        public int Description { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [MaxLength(100)]
+        public DateTime Date { get; set; }
+        public bool Done { get; set; }
+    }
+}
